72553f19f0c4dcab24c10dce7575c4213cc5c2d6
Andrew <androden@yandex.ru>
2024-02-20 17:58:22 +0300

    Created PublisherControllers with GET, POST, PUT and DELETE methods

---
+++ Models/PublisherController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace BookAPI.Models
+{
+    [ApiController]
+    public class PublisherController: ControllerBase
+    {
+        private ApplicationContext _context;
+        public PublisherController(ApplicationContext context) { _context = context; }
+
+        [HttpGet(Name = "GetPublishers")]
+        public List<Publisher> Get() 
+        {
+            return _context.Publishers.ToList();
+        }
+
+        [HttpPost(Name = "AddPublishers")]
+        public void Post(Publisher publisher) 
+        {
+            _context.Publishers.Add(publisher);
+            _context.SaveChanges();
+        }
+
+        [HttpPut(Name = "EditPublishers")]
+        public void Put(Publisher publisher) 
+        {
+            _context.Publishers.Update(publisher);
+            _context.SaveChanges();
+        }
+
+        [HttpDelete(Name = "EditPublishers")]
+        public void Delete(int pId) 
+        {
+            var deletePublisher = _context.Publishers.FirstOrDefault(p => p.Id == pId);
+            _context.Publishers.Remove(deletePublisher);
+            _context.SaveChanges();
+        }
+    }
+}
