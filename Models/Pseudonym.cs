13ebf8ac301a80a38b2f999e4b0d1d3dc583a56c
Andrew <androden@yandex.ru>
2024-02-27 00:23:08 +0300

    Brougth Publisher, Book and Pseudonym model to the same form

--- Models/Pseudonym.cs
+++ Models/Pseudonym.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public Author Author { get; set; }
-        public int AuthorID { get; set; }
+        public Author? Author { get; private set; }
+        public int? AuthorID { get; private set; }
     }
 }
